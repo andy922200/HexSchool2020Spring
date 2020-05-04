6b48afc73e54984b26d6c67086082769bd4ba092
andysml <andy922200@gmail.com>
2020-05-04 16:02:07 +0800

    finish faq pages

--- Week3_Week4/week3_week4.js
+++ Week3_Week4/week3_week4.js
@@ -1,4 +1,17 @@
 $('.ham-icon').click(function (e) {
   e.preventDefault();
-  $('.dropdown-menu').toggleClass('active')
+  $('.dropdown-menu').toggleClass('active');
+})
+
+/*default all hide, except 1st card*/
+$('.card-item-body').hide();
+$('.card-item').eq(0).addClass('active');
+$('.card-item-body').eq(0).show();
+
+// click li
+$('.card-item').click(function(e){
+  e.preventDefault();
+  $(this).toggleClass('active').siblings().removeClass('active');
+  $(this).children().next().slideToggle();
+  $(this).siblings().children().next().slideUp();
 })
